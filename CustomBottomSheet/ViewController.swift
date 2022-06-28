3713916f6b587404e8185acecece74ea45eb3a73
blakeCS <mburton@blakeschool.org>
2022-06-28 14:15:03 -0500

    Initial Commit

---
+++ CustomBottomSheet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomBottomSheet
+//
+//  Created by Madeline Burton on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomBottomSheetUITests/CustomBottomSheetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CustomBottomSheetUITestsLaunchTests.swift
+//  CustomBottomSheetUITests
+//
+//  Created by Madeline Burton on 6/28/22.
+//
+
+import XCTest
+
+class CustomBottomSheetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
